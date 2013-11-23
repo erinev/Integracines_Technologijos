705c2b6611a9c80d595806aa113e0a09629dcf65
Erikas Neverdauskas <erikasnever@hotmail.com>
2013-11-23 19:59:07 +0200

    Added WebService project

---
+++ KTU.Integracines_Technologijos/1_Laboras/ErikoWebServisas/ManoWebServisas.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="ManoWebServisas.asmx.cs" Class="ErikoWebServisas.ManoWebServisas" %>
---
+++ KTU.Integracines_Technologijos/1_Laboras/ErikoWebServisas/ManoWebServisas.asmx.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Web.Services;
+
+namespace ErikoWebServisas
+{
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [ToolboxItem(false)]
+    public class ManoWebServisas : WebService
+    {
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
